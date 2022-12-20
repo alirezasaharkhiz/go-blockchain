463f7bd8661fdbc31628ae2da290d8ecb2727c16
Alireza Saharkhiz <alireza.saharkhiz@outlook.com>
2022-12-20 22:09:07 +0330

    Add test run for wallets on main

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"go-blockchain/blockchain"
+	"go-blockchain/wallet"
 	"log"
 )
 
@@ -10,29 +10,30 @@ func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
+//func main() {
+//	blockchainAddress := "my_blockchain_address"
+//	bc := blockchain.NewBlockchain(blockchainAddress)
+//
+//	bc.AddTransaction("A", "B", 1.0)
+//	bc.AddTransaction("x", "z", 5.0)
+//	bc.AddTransaction("x", "A", 7.1)
+//	bc.Mining()
+//
+//	bc.AddTransaction("c", "g", 5.0)
+//	bc.AddTransaction("c", "f", 7.1)
+//	bc.Mining()
+//
+//	bc.Print()
+//
+//	fmt.Println("c", bc.CalculateBalance("c"))
+//	fmt.Println("f", bc.CalculateBalance("f"))
+//	fmt.Println("A", bc.CalculateBalance("A"))
+//	fmt.Println("x", bc.CalculateBalance("x"))
+//}
+
 func main() {
-	blockchainAddress := "my_blockchain_address"
-	bc := blockchain.NewBlockchain(blockchainAddress)
-
-	bc.AddTransaction("A", "B", 1.0)
-	bc.AddTransaction("x", "z", 5.0)
-	bc.AddTransaction("x", "A", 7.1)
-	bc.Mining()
-	//lastBlockHash := bc.LastBlock().Hash256()
-	//nonce := bc.GenerateProofOfWorkNonce()
-	//bc.AddBlock(nonce, lastBlockHash)
-
-	bc.AddTransaction("c", "g", 5.0)
-	bc.AddTransaction("c", "f", 7.1)
-	bc.Mining()
-	//lastBlockHash = bc.LastBlock().Hash256()
-	//nonce = bc.GenerateProofOfWorkNonce()
-	//bc.AddBlock(nonce, lastBlockHash)
-
-	bc.Print()
-
-	fmt.Println("c", bc.CalculateBalance("c"))
-	fmt.Println("f", bc.CalculateBalance("f"))
-	fmt.Println("A", bc.CalculateBalance("A"))
-	fmt.Println("x", bc.CalculateBalance("x"))
+	w := wallet.NewWallet()
+	fmt.Println(w.GetPrivateKeyAsString())
+	fmt.Println(w.GetPublicKeyAsString())
+	fmt.Println(w.GetBlockchainAddress())
 }
