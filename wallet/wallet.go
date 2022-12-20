6940564e5f533d6fddf33d48ccbfb18431fea558
Alireza Saharkhiz <alireza.saharkhiz@outlook.com>
2022-12-21 00:18:58 +0330

    Remove self comments (complex codes remain complex)

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -22,34 +22,33 @@ func NewWallet() *Wallet {
 	w.privateKey = prk
 	w.publicKey = &w.privateKey.PublicKey
 
-	//SHA256 hashing (result 32 bytes)
 	enc1 := sha256.New()
 	enc1.Write(w.publicKey.X.Bytes())
 	enc1.Write(w.publicKey.Y.Bytes())
 	dh1 := enc1.Sum(nil)
-	//RIPEMD160 hashing on SHA256 (result 20 bytes)
+
 	enc2 := ripemd160.New()
 	enc2.Write(dh1)
 	dh2 := enc2.Sum(nil)
-	//Add version bytes of main network (version: 0x00)
+
 	vb := make([]byte, 21)
 	vb[0] = 0x00
 	copy(vb[1:], dh2[:])
-	//Add another SHA256 again
+
 	enc4 := sha256.New()
 	enc4.Write(vb)
 	dh4 := enc4.Sum(nil)
-	//Add another one
+
 	enc5 := sha256.New()
 	enc5.Write(dh4)
 	dh5 := enc5.Sum(nil)
-	//Get first 4 bytes for checksum
+
 	chs := dh5[:4]
-	//Add checksum bytes at the end of version byte digested hash (result 25 bytes)
+
 	dh6 := make([]byte, 25)
 	copy(dh6[:21], vb[:])
 	copy(dh6[21:], chs[:])
-	//Convert to base58 string
+
 	adr := base58.Encode(dh6)
 	w.blockchainAddress = adr
 
