73b6888c891e32dc51c1efd228eeed7babc44fda
Alireza Saharkhiz <alireza.saharkhiz@outlook.com>
2022-12-23 23:51:32 +0330

    Update Mining function

--- blockchain/mine.go
+++ blockchain/mine.go
@@ -7,8 +7,8 @@ const MiningSender = "Main Chain"
 const MiningReward = 25
 
 func (bc *Blockchain) Mining() bool {
-	bc.AddTransaction(MiningSender, bc.address, MiningReward)
-	nonce := bc.GenerateProofOfWorkNonce()
+	bc.AddTransaction(MiningSender, bc.address, MiningReward, nil, nil)
+	nonce := bc.GenerateProofOfWorkValidNonce()
 	ph := bc.LastBlock().Hash256()
 	bc.AddBlock(nonce, ph)
 	log.Println("action=mining, status=success")
